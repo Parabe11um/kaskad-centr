76e5e76294e3c9c3e1b2bd7bf7e569181e3c70a7
Parabe11um <r.a.bazhenoff@gmail.com>
2025-04-21 23:33:54 +0300

    logo centered in menu

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,5 +1,5 @@
 <div class="text-gray-800 font-semibold">
     <span class="text-blue-500 text-2xl">
-        <img src="{{ asset('images/logo.jpg') }}" alt="Логотип Каскад" style="height: 4rem;">
+        <img class="logo-mark" src="{{ asset('images/logo.jpg') }}" alt="Логотип Каскад" style="height: 4rem;">
     </span>
 </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,11 +5,7 @@
         hidden: false,
         init() {
             window.addEventListener('scroll', () => {
-                if (window.scrollY > this.scrollY) {
-                    this.hidden = true;
-                } else {
-                    this.hidden = false;
-                }
+                this.hidden = window.scrollY > this.scrollY;
                 this.scrollY = window.scrollY;
             });
         }
@@ -17,79 +13,77 @@
     x-bind:class="hidden ? '-translate-y-full' : 'translate-y-0'"
     class="bg-white border-b border-gray-200 fixed top-0 left-0 right-0 z-50 shadow transition-transform duration-300"
 >
-
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <!-- Левая часть: логотип -->
-            <div class="flex">
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-mark />
-                    </a>
-                </div>
-                <!-- Главное меню (скрыто на мобильных) -->
-                <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
-                        {{ __('Главная') }}
-                    </x-nav-link>
-                    <x-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')">
-                        {{ __('Новости') }}
-                    </x-nav-link>
-                    <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
-                        {{ __('О центре') }}
-                    </x-nav-link>
-                    <x-nav-link href="{{ route('contacts') }}" :active="request()->routeIs('contacts')">
-                        {{ __('Контакты') }}
-                    </x-nav-link>
-                </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative h-32 flex items-center justify-between">
+        <!-- Левая часть -->
+        <div class="flex items-center space-x-8">
+            <div class="hidden sm:flex space-x-8">
+                <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                    {{ __('Главная') }}
+                </x-nav-link>
+                <x-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')">
+                    {{ __('Новости') }}
+                </x-nav-link>
+                <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
+                    {{ __('О центре') }}
+                </x-nav-link>
+                <x-nav-link href="{{ route('contacts') }}" :active="request()->routeIs('contacts')">
+                    {{ __('Контакты') }}
+                </x-nav-link>
             </div>
+        </div>
 
-            <!-- Правая часть: авторизация -->
-            <div class="hidden sm:ml-6 sm:flex sm:items-center">
-                @auth
-                    @include('layouts.partials.header-right-auth')
-                @else
-                    @include('layouts.partials.header-right-guest')
-                @endauth
-            </div>
+        <!-- Центр: логотип -->
+        <div class="absolute left-1/2 transform -translate-x-1/2">
+            <a href="{{ route('home') }}" class="flex items-center">
+                <x-application-mark />
+            </a>
+        </div>
 
-            <!-- Мобильное меню (гамбургер) -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = !open" type="button"
-                        class="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span class="sr-only">Открыть меню</span>
-                    <svg x-show="!open" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                    <svg x-show="open" x-cloak class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+        <!-- Правая часть -->
+        <div class="hidden sm:flex items-center">
+            @auth
+                @include('layouts.partials.header-right-auth')
+            @else
+                @include('layouts.partials.header-right-guest')
+            @endauth
+        </div>
+
+        <!-- Мобильный гамбургер -->
+        <div class="sm:hidden flex items-center">
+            <button @click="open = !open" type="button"
+                    class="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span class="sr-only">Открыть меню</span>
+                <svg x-show="!open" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <svg x-show="open" x-cloak class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
         </div>
     </div>
 
     <!-- Мобильное меню -->
     <div x-show="open" x-cloak class="sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="block my-4 border-transparent text-base font-medium text-gray-600 hover:border-gray-300 hover:text-gray-800">
+        <div class="pt-2 pl-4 pb-3 space-y-1">
+            <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="block px-4 py-2">
                 {{ __('Главная') }}
             </x-nav-link>
-            <x-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')" class="block my-4 border-transparent text-base font-medium text-gray-600 hover:border-gray-300 hover:text-gray-800">
+            <x-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')" class="block px-4 py-2">
                 {{ __('Новости') }}
             </x-nav-link>
-            <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')" class="block my-4 border-transparent text-base font-medium text-gray-600 hover:border-gray-300 hover:text-gray-800">
+            <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')" class="block px-4 py-2">
                 {{ __('О центре') }}
             </x-nav-link>
-            <x-nav-link href="{{ route('contacts') }}" :active="request()->routeIs('contacts')">
+            <x-nav-link href="{{ route('contacts') }}" :active="request()->routeIs('contacts')" class="block px-4 py-2">
                 {{ __('Контакты') }}
             </x-nav-link>
         </div>
-        <div class="pt-4 pb-3 border-t border-gray-200">
+        <div class="pt-4 pl-6 pb-3 border-t border-gray-200">
             @auth
                 @include('layouts.partials.header-right-auth')
             @else
